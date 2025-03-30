2cf0046da7657ede3c299c19c4c11ed7f5c4e75f
Rheetham Menon <rheethammenon@gmail.com>
2025-03-30 14:20:03 +0530

    for demo

---
+++ 1-fundamentals/ForDemo.java
@@ -0,0 +1,14 @@
+/*
+ Demonstrate the for loop.
+*/
+
+class ForDemo {
+    public static void main(String args[]) {
+        int count;
+
+        for (count = 0; count < 5; count = count + 1)
+            System.out.println("This is count: " + count);
+
+        System.out.println("Done!");
+    }
+}
