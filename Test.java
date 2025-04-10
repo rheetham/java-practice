e42423266079dd0844a1da1996e0e7d2750dc375
Rheetham Menon <rheethammenon@gmail.com>
2025-04-10 09:35:55 +0530

    modified the test file

--- Test.java
+++ Test.java
@@ -1,5 +1,9 @@
 class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws java.io.IOException {
+        char ch;
 
+        ch = (char) System.in.read();
+
+        System.out.println(ch);
     }
 }
