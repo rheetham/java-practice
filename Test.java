9c06822ccd1a665256fdbeacd90beb99e7e04595
Rheetham Menon <rheethammenon@gmail.com>
2025-07-18 11:26:52 +0530

    nqt training

--- Test.java
+++ Test.java
@@ -1,9 +1,4 @@
 class Test {
-    public static void main(String[] args) throws java.io.IOException {
-        char ch;
-
-        ch = (char) System.in.read();
-
-        System.out.println(ch);
+    public static void main(String[] args) {
     }
 }
---
+++ nqt-training/Main.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+    }
+
+    int main() {
+        return 1;
+    }
+}
